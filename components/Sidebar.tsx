f43db5d85fb5ddba693476ca20cbb647ae44e3a4
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2025-01-30 17:17:50 +0530

    made the funtionality of the sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,51 @@
+import React, { use } from 'react'
+// import { useRouter } from "next/navigation"
+import { NavigationContext } from '@/lib/NavigationProvider';
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
+import { PlusIcon } from '@radix-ui/react-icons';
+// import { useMutation, useQuery } from 'convex/react';
+
+
+const Sidebar = () => {
+    // const router = useRouter();
+    const { closeMobileNav, isMobileNavOpen } = use(NavigationContext)
+
+// const chats = useQuery(api.chats.listChats);
+// const CreateChat = useMutation(api.chats.createChat);
+// const deleteChat = useMutation(api.chats.deleteChat)
+
+// const handleClick = ()=>{
+    //TODO :- Route to chat Id page
+    // router.push('/dashboard/chat')
+    // closeMobileNav();
+// }
+
+    return <>
+        {isMobileNavOpen && (
+            <div className='fixed inset-0 bg-black/20 z-40 md:hidden'
+                onClick={closeMobileNav}
+            />
+        )}
+        <div className={cn(
+            "fixed md:inset-y-0 top-14 bottom-0 left-0 z-50 w-72 bg-gray-50/80 backdrop-blur-xl border-r border-gray-200/50 shadow-sm hover:shadow transition-all duration-200 transform transition-transform duration-300 ease-in-out md:relative md:translate-x-0 md:top-0 flex flex-col",
+            isMobileNavOpen ? "translate-x-0" : "-translate-x-full"
+        )}>
+            <div className='p-4 border-b border-gray-200/50'>
+                <Button className='w-full bg-white hover:bg-gray-50 text-gray-700 border border-gray-200/50 shadow-sm hover:shadow transition-all duration-200'
+                    // onClick={handleNewChat}
+                    >
+                    <PlusIcon className='mr-2 h-4 w-4' />New Gossip
+                </Button>
+            </div>
+
+            <div className='flex-1 overflow-y-auto space-y-2.5 p-4 scrollbar-thin scrollbar-thumb-gray-200 scroll-track-transparent' >
+                {/* {chat?.map((chat)=>(
+                    <ChatRow key={chat.id} chat={chat} onDelete={handleDeleteChat} />
+                ))} */}
+            </div>
+        </div>
+    </>
+}
+
+export default Sidebar
